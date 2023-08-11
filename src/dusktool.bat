7e9b1f14be92153df9f4820d4d5d94a7aa4d47ad
XDuskAshes <thegaymeradummy@gmail.com>
2023-08-11 05:13:27 -0400

    Added prog info, fixed small bug
    Sometimes when you press enter when selecting, it would use the last
    selected item.

--- src/dusktool.bat
+++ src/dusktool.bat
@@ -1,11 +1,12 @@
 @echo off
 
-set name=DuskTool 1.0.0
+set name=DuskTool 1.0.1
 
 title %name%
 
 goto tool
 
+:: Temp files cleanup.
 :TempClean
 cd "C:\Windows\Temp\"
 del "*"
@@ -14,41 +15,58 @@ del "*"
 pause
 goto tool
 
+:: Ping a place
 :pinger
 set /p choice="ping what? "
 ping %choice%
 pause
 goto tool
 
+:: Grab system info.
+:: TODO: Make this custom.
 :sysinfo
 systeminfo
 pause
 goto tool
 
+:: ipconfig /all lmao
 :ipstuff
 ipconfig /all
 pause
 goto tool
 
+:: Program info
+:info
+echo %name%
+echo By Dusk
+echo Functionality:
+echo Clean temp files, ping internet addresses, view sysinfo, see port info
+pause
+goto tool
+
 :tool
 cls
+echo %name%
 echo 1 - Clean temp files.
 echo 2 - Ping an address.
 echo 3 - System info.
 echo 4 - Run the 'ipconfig' command.
-echo 5 - Exit.
-
-set /p choice=choice: 
+echo 5 - Program info.
+echo 6 - Exit.
+set choice="0"
+set /p choice=Enter one of the above numbers: 
 
 if "%choice%"=="1" (
     call :TempClean
 ) else if "%choice%"=="2" (
     call :pinger
-) else if "%choice%" == "3" (
+) else if "%choice%"=="3" (
     call :sysinfo
-) else if "%choice%" == "4" (
+) else if "%choice%"=="4" (
     call :ipstuff
 ) else if "%choice%"=="5" (
+    call :info
+) else if "%choice%"=="6" (
     echo Exiting.
     pause
     exit
