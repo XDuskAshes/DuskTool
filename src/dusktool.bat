ecd0acb7862c4d38879cfbdc9f12784e4f5e604a
XDuskAshes <thegaymeradummy@gmail.com>
2023-08-10 22:27:47 -0400

    added useful functions

--- src/dusktool.bat
+++ src/dusktool.bat
@@ -4,10 +4,65 @@ set name=DuskTool 1.0.0
 
 title %name%
 
+goto tool
+
 :genericError
-call popup\error.bat DuskTool genericError Cancel.
-exit
+if exist "popup\error.bat" (
+    call popup\error.bat DuskTool genericError Cancel.
+    exit
+) else (
+    title DuskTool encountered a generic error
+    echo A generic error occured, and the error program couldn't be found.
+    ping localhost -n 10 >nul
+    exit
+)
 
-goto genericError
-
+:TempClean
+cd "C:\Windows\Temp\"
+del "*"
+cd "%UserProfile%\AppData\Local\Temp\"
+del "*"
 pause
+goto tool
+
+:pinger
+set /p choice="ping what? "
+ping %choice%
+pause
+goto tool
+
+:sysinfo
+systeminfo
+pause
+goto tool
+
+:ipstuff
+ipconfig /all
+pause
+goto tool
+
+:tool
+cls
+echo 1 - Clean temp files.
+echo 2 - Ping an address.
+echo 3 - System info.
+echo 4 - Run the 'ipconfig' command.
+echo 5 - Exit.
+
+set /p choice=choice: 
+
+if "%choice%"=="1" (
+    call :TempClean
+) else if "%choice%"=="2" (
+    call :pinger
+) else if "%choice%" == "3" (
+    call :sysinfo
+) else if "%choice%" == "4" (
+    call :ipstuff
+) else if "%choice%"=="5" (
+    echo Exiting.
+    pause
+    exit
+)
+
+goto tool
--- src/popup/error.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-title %1 has encountered an error.
-
-echo Error details: %2
-echo The program will now: %3
-
-pause
